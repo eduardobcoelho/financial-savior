8dd24948376f71ad0aa01ace806e5f0ef61aef14
Luiz Eduardo Barros Coelho <luizbcoelho27@outlook.com>
2025-05-10 01:10:55 -0300

    limpando arquivos ignorados

--- backend/dist/users/controller/users.controller.d.ts
+++
@@ -1,7 +0,0 @@
-import { CreateUserDto } from '../dto/create-user-dto';
-import { ICreateUserService } from '../service/create-user/create-user.service';
-export declare class UsersController {
-    private readonly createUserService;
-    constructor(createUserService: ICreateUserService);
-    create(input: CreateUserDto): Promise<import("../entity/user.entity").UserEntity>;
-}
