48a26ec9ff3b7b6270034b11983fdb318300211b
Luiz Eduardo Barros Coelho <luizbcoelho27@outlook.com>
2025-05-13 21:02:28 -0300

    feat: implemented bcrypt in password

--- backend/src/users/service/create-user/create-user.service.ts
+++ backend/src/users/service/create-user/create-user.service.ts
@@ -4,9 +4,10 @@ import { UserEntity } from 'src/users/entity/user.entity';
 import { IUserRepository } from 'src/users/repository/user.repository';
 import { FindUserByEmailService } from '../find-user-by-email/find-user-by-email.service';
 import { ValidationMessages } from 'src/users/enum';
+import bcrypt from 'bcrypt';
 
 export interface ICreateUserService {
-  exec: (data: CreateUserDto) => Promise<UserEntity>;
+  exec: (data: CreateUserDto) => Promise<Omit<UserEntity, 'password'>>;
 }
 
 @Injectable()
@@ -20,7 +21,7 @@ export class CreateUserService implements ICreateUserService {
   ) {}
 
   async exec(data: CreateUserDto) {
-    let user: UserEntity | null = null;
+    let user: Omit<UserEntity, 'password'> | null = null;
 
     try {
       user = await this.findUserByEmailService.exec(data.email);
@@ -29,6 +30,12 @@ export class CreateUserService implements ICreateUserService {
     }
 
     if (user) throw new BadRequestException(ValidationMessages.emailInvalid);
-    return await this.userRepository.create(data);
+
+    const passwordBcrypted = bcrypt.hashSync(data.password, 10);
+
+    return await this.userRepository.create({
+      ...data,
+      password: passwordBcrypted as string,
+    });
   }
 }
