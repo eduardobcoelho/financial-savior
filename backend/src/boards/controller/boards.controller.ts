b73d6a55717740e22cdd0b516e456fd9efa98a84
Luiz Eduardo Barros Coelho <luizbcoelho27@outlook.com>
2025-10-12 00:18:33 -0300

    feat: add update in board

--- backend/src/boards/controller/boards.controller.ts
+++ backend/src/boards/controller/boards.controller.ts
@@ -7,6 +7,7 @@ import {
   Param,
   ParseIntPipe,
   Post,
+  Put,
   UseGuards,
   ValidationPipe,
 } from '@nestjs/common';
@@ -14,6 +15,8 @@ import { AuthJwtTokenGuard } from 'src/auth/guard/jwt-token/jwt-token.guard';
 import { CreateBoardDto } from '../dto/create-board.dto';
 import { ICreateBoardService } from '../service/create-board/create-board.service';
 import { IDeleteBoardService } from '../service/delete-board/delete-board.service';
+import { UpdateBoardDto } from '../dto/update-board.dto';
+import { IUpdateBoardService } from '../service/update-board/update-board.service';
 
 @Controller('boards')
 @UseGuards(AuthJwtTokenGuard)
@@ -24,6 +27,9 @@ export class BoardsController {
 
     @Inject('IDeleteBoardService')
     private readonly deleteBoardService: IDeleteBoardService,
+
+    @Inject('IUpdateBoardService')
+    private readonly updateBoardService: IUpdateBoardService,
   ) {}
 
   @Post()
@@ -37,5 +43,10 @@ export class BoardsController {
   @Delete(':id')
   async delete(@Param('id', ParseIntPipe) id: number) {
     return await this.deleteBoardService.exec(id);
+  }
+
+  @Put(':id')
+  async update(@Param('id', ParseIntPipe) id: number, data: UpdateBoardDto) {
+    return await this.updateBoardService.exec(id, data);
   }
 }
